68d3906e526fda33a5127007d3ece9853cb2f615
Kkubb <kubinskaiaa@gmail.com>
2021-01-11 02:15:00 +0300

    Update hw24.java

--- lesson24/hw24.java
+++ lesson24/hw24.java
@@ -1,4 +1,5 @@
 package lesson24;
+
 /* 1) Даны два числа, a и b. Верните большее из них. Не используйте никаких методов из библиотеки Math. Покажите работу тернарного оператора.
         getMeMax(15, 5) → 15
         getMeMax(4, 16) → 16
@@ -11,63 +12,61 @@ package lesson24;
         myFirstCompareTo("Berlin", "Moscow"); → "Berlin"
         myFirstCompareTo("Minsk", "Moscow"); → "Minsk"
         myFirstCompareTo("Kyiv", "Paris"); → "Kyiv"
-        myFirstCompareTo("Могилёв", "Потсдам"); → "Могилёв"*/
-
+        myFirstCompareTo("Могилёв", "Потсдам"); → "Могилёв"
+   4) While, Do While
+        Решите не используя цикл for
+        Даны целые числа A и B, где B > 0. Вывести B раз число A.
+        Вывести в порядке возрастания все числа между А и B, где а = 5, а B = 17.*/
 public class hw24 {
     public static void main(String[] args) {
-    //1)
+        //1)
         System.out.println(biggerThanOther(15, 5));// → 15
         System.out.println(biggerThanOther(4, 16));//→ 16
         System.out.println(biggerThanOther(20, 100));// → 100
-    //2)
+        //2)
         System.out.println(smallerThanOther(15, 45));// → 15
         System.out.println(smallerThanOther(44, 16));//→ 16
         System.out.println(smallerThanOther(200, 100));// → 100
-    //3)
+        //3)
         System.out.println(compareLetters("Berlin", "Moscow"));// → "Berlin"
         System.out.println(compareLetters("Minsk", "Moscow"));// → "Minsk"
         System.out.println(compareLetters("Kyiv", "Paris"));// → "Kyiv"
         System.out.println(compareLetters("Могилёв", "Потсдам"));// → "Могилёв"
-        //While, Do While
-        //Решите не используя цикл for
-        //Даны целые числа A и B, где B > 0. Вывести B раз число A.
-        doNotUseFor(10, 5);
-        //Вывести в порядке возрастания все числа между А и B, где а = 5, а B = 17.
-        doNotUseForTwo(5, 17);
-
+        //4)
+        whileNumbers(7, 7);
+        //5)
+        growingNumbers(5, 17);
     }
-    private static int smallerThanOther(int a, int b) {
+
+    public static int smallerThanOther(int a, int b) {
         return (a < b) ? a : b;
     }
-    private static int biggerThanOther(int a, int b) {
+
+    public static int biggerThanOther(int a, int b) {
         return (a > b) ? a : b;
     }
-    private static void doNotUseForTwo(int a, int b) {
-        do {
-            System.out.println(a);;
-            a++;
-        } while (a < b);
-    }
-
-    private static void doNotUseFor(int a, int b) {
-        do {
-            System.out.println(a);
-            b--;
-        } while (b > 0);
-    }
-
-    private static String compareLetters(String firstCity, String secondCity) {
+    public static String compareLetters(String firstCity, String secondCity) {
         if (firstCity.charAt(0) < secondCity.charAt(0) || firstCity.charAt(1) < secondCity.charAt(1)) {
             return firstCity;
         } else {
             return secondCity;
         }
     }
+    public static void whileNumbers(int a, int b) {
+        int i = 0;
+        if (a > 0 && b > 0) {
+            do {
+                System.out.print(a);
+                i++;
+            } while (i < b);
+        }
+    }
 
-
-}
-
-
-
+    public static void growingNumbers(int a, int b) {
+        do {
+            System.out.println(a);
+            a++;
+        } while (a < b);
+    }
 
 }
